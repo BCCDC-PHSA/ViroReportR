2cab780f79121e092e66d7d2087f8d1d1f7796d9
Caesar W <caesar.wong@bccdc.ca>
2023-10-27 11:33:20 -0700

    adding testthat for phrdw data transformation

---
+++ tests/testthat/test_phrdw_data_transformation.R
@@ -0,0 +1,110 @@
+# load phrdw data --------------------------------------------------------
+
+data_folder <- "O:/BCCDC/Groups/Analytics/Projects/covid_modeling/06 Projects/RSV Flu Modelling/Data"
+
+vpp_only_data_file <- file.path(data_folder,"phrdw_flu_daily_count_vpp_only.rData")
+
+load(vpp_only_data_file) # dataframe named: phrdw_flu_daily_count
+
+
+# Run Package Data Transformation Function --------------------------------
+
+weekly_phrdw_data <- get_weekly_phrdw(phrdw_flu_daily_count)
+
+disease_type <- "rsv"
+
+weekly_phrdw_type_date_age <- get_weekly_phrdw_by_type_date_age(
+  weekly_phrdw_data = weekly_phrdw_data,
+  type = disease_type,
+  start_date = "2023-01-01",
+  end_date = "2023-02-01",
+  start_age = 0,
+  end_age = 10)
+
+# test data transformation function ---------------------------------------
+
+test_that("weekly phrdw column name correct", {
+  expect_equal(colnames(weekly_phrdw_data), c("date","age_years","sars_cov2","rsv","flu_a","flu_b"))
+})
+
+
+test_that("weekly phrdw filtered column name correct", {
+  expect_equal(colnames(weekly_phrdw_type_date_age), c("date", "confirm"))
+})
+
+
+# test function error handling --------------------------------------------
+
+test_that("invalid disease type handling correct", {
+
+  wrong_disease_type <- 'abc'
+
+  expect_error(get_weekly_phrdw_by_type_date_age(
+    weekly_phrdw_data = weekly_phrdw_data,
+    type = wrong_disease_type,
+    start_date = "2023-01-01",
+    end_date = "2023-02-01",
+    start_age = 0,
+    end_age = 10),
+    "invalid disease type, available options: 'sars_cov2', 'rsv', 'flu_a', 'flu_b'")
+})
+
+
+test_that("invalid start date handling correct", {
+
+  wrong_start_date <- '2024-01-01'
+
+  expect_error(get_weekly_phrdw_by_type_date_age(
+    weekly_phrdw_data = weekly_phrdw_data,
+    type = disease_type,
+    start_date = wrong_start_date,
+    end_date = "2023-02-01",
+    start_age = 0,
+    end_age = 10),
+    "start date is later than the end date")
+})
+
+
+test_that("invalid start date handling correct", {
+
+  wrong_start_date <- '2024-01-01'
+
+  expect_error(get_weekly_phrdw_by_type_date_age(
+    weekly_phrdw_data = weekly_phrdw_data,
+    type = disease_type,
+    start_date = wrong_start_date,
+    end_date = "2023-02-01",
+    start_age = 0,
+    end_age = 10),
+    "start date is later than the end date")
+})
+
+
+test_that("invalid start age handling correct", {
+
+  wrong_start_age <- 200
+
+  expect_error(get_weekly_phrdw_by_type_date_age(
+    weekly_phrdw_data = weekly_phrdw_data,
+    type = disease_type,
+    start_date = "2023-01-01",
+    end_date = "2023-02-01",
+    start_age = wrong_start_age,
+    end_age = 10),
+    "start age is later than the end age")
+})
+
+
+test_that("invalid start age data type handling correct", {
+
+  wrong_start_age <- "123"
+
+  expect_error(get_weekly_phrdw_by_type_date_age(
+    weekly_phrdw_data = weekly_phrdw_data,
+    type = disease_type,
+    start_date = "2023-01-01",
+    end_date = "2023-02-01",
+    start_age = wrong_start_age,
+    end_age = 10),
+    "start age or end age is not numeric")
+})
