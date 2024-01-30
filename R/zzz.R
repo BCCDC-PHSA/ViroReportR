0313d6f32e8be48d555759b47101ed3030fb9e01
ntmv <niru_tmv@MacBook-Pro.local>
2024-01-30 11:47:22 -0800

    #27 welcome message pointing to website

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "================================================\n",
+    "Welcome to vriforecasting! \n",
+    "Please run `pkgdown::build_site(lazy = TRUE)` in your console \n",
+    "to access documentation on the package website \n",
+    "================================================"
+    )
+}
