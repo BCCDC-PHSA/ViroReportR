6bf858f803dbf18790deba6e01ac9caaea23727b
Rebeca Falcao <rebeca.falcao@bccdc.ca>
2026-01-16 11:07:14 -0800

    remove zzz.R file with startup message

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    "\n-- ViroReportR -----------------------------------------------\n",
-    "Run `pkgdown::build_site(lazy = TRUE)` to access documentation\n",
-    "--------------------------------------------------------------"
-  )
-}
